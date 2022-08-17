977d98d9b0d1f345817515a1cb1d18bfce76e586
AdilSoipov <111345038+AdilSoipov@users.noreply.github.com>
2022-08-17 22:39:03 +0500

    Update SalaryCalculator.java

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -17,4 +17,3 @@ public class SalaryCalculator {
         }
     }
 }
-Basic Salary <= 10000: taxes - 15%. 10000 < Basic Salary <= 20000: taxes - 18% Basic Salary > 20000 : taxes - 20% Basic salary < 0 -> "wrong input!"
